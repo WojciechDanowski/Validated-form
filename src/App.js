7a631e5fe78bf36420a91059f78fcfaebcbf6514
Wojciech Danowski <wojciechdanowskiz@gmail.com>
2020-04-14 09:10:46 +0200

    Created a sctructure with some css

--- src/App.js
+++ src/App.js
@@ -2,14 +2,53 @@ import React, { Component } from "react";
 import "./App.css";
 
 class App extends Component {
-  state = {};
+  state = {
+    username: "",
+    email: "",
+    password: "",
+  };
+
+  handleChange = (e) => {
+    const value = e.target.value;
+
+    this.setState({
+      username: value,
+    });
+  };
+
   render() {
     return (
       <div className="App">
         <form>
           <label htmlFor="user">
             Your name:
-            <input type="text" id="user" name="username" />
+            <input
+              type="text"
+              id="user"
+              name="username"
+              value={this.state.username}
+              onChange={this.handleChange}
+            />
+          </label>
+          <label htmlFor="email">
+            Your email:
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={this.state.email}
+              onChange={this.handleChange}
+            />
+          </label>
+          <label htmlFor="password">
+            Your passs:
+            <input
+              type="password"
+              id="password"
+              name="password"
+              value={this.state.password}
+              onChange={this.handleChange}
+            />
           </label>
         </form>
       </div>
